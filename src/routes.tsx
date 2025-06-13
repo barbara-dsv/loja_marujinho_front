bb4e9c6e5dd3cde1ae2b0c18e621d55295b250b2
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-06-12 22:24:33 -0300

    first commit

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
+export const routes = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route
+                path="/"
+            />
+        </Route>
+    )
+)
