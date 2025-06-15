7f51917e1655f9565ab92bfd24d46f447282388a
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-06-15 10:49:47 -0300

    :construction: feat: comecando tela de pedido

---
+++ src/pages/Pedido/index.tsx
@@ -0,0 +1,8 @@
+export const Pedido: React.FC = () => {
+
+    return (
+        <div >
+            <h1>tela de pedido</h1>
+        </div>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { Pedido } from "./pages/Pedido";
 
 export const routes = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route
                 path="/"
+                element={<Pedido />}
             />
         </Route>
     )
