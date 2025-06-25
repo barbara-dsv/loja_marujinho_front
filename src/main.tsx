f1cc5e74a39c5dcc1b33b8d884a1553b8d2addad
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-06-24 22:54:25 -0300

    :boom:	fix: concertando importação

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes'
-import './index.css'
+import './global.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={routes} />
