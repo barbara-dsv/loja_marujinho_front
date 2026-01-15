fc0178943624793b894424d42314a76043380ac5
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-01-14 22:16:34 -0300

    :sparkles:	feat: adicionado bundle

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './global.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
