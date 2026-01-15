7cb995bf014e476d2d71ae2cac7883b5c4ab2557
Bárbara Lourenço Tavares <babisolua@gmail.com>
2026-01-14 22:18:12 -0300

    :recycle:	refactor : refatorando página utilizando bootstrap

--- src/pages/Pedido/index.tsx
+++ src/pages/Pedido/index.tsx
@@ -1,41 +1,37 @@
-import { ArrowRightIcon, ChatCircleIcon, MapPinIcon, ShoppingCartIcon } from '@phosphor-icons/react';
 import styles from './pedido.module.css';
-import capa from '../../assets/capa_site.png'
 export const Pedido: React.FC = () => {
 
     return (
-        <div className={styles.container} >
+        <div className={styles.conteiner}>
             <header className={styles.header_pedido}>
                 <div className={styles.header_icone}>
-                    <ChatCircleIcon size={18} />
-                    <h1>Marujinho</h1>
+                    <i className={`bi bi-emoji-wink-fill ${styles.emoji_wink}`}></i>
+                    <h1 className={styles.titulo}>Marujinho</h1>
                 </div>
-                <ShoppingCartIcon size={18} color='#8C8D8BFF' />
+                <i className="bi bi-cart"></i>
             </header>
-
             <hr />
-
             <div className={styles.capa}>
-                <img src={capa} alt="capa do site" className={styles.img_capa} />
+                <img src="https://res.cloudinary.com/doymbnklx/image/upload/v1752708290/marujinho/xrikkwj08sgwiannuwxa.png" alt="capa do site" className={styles.img_capa} />
                 <div className={styles.aviso}>
-                    <MapPinIcon size={18} />
+                    <i className="bi bi-geo-alt"></i>
                     <span>
                         <p className={styles.aviso_importante}>Aviso Importante!</p>
                         <p className={styles.aviso_entregas}>Entregas apenas no bairro Mucuripe, a pé, garantindo frescor e agilidade.</p>
                     </span>
                 </div>
             </div>
-
+            <hr />
             <section className={styles.carrinho}>
                 <div className={styles.qtd_pedido}>
-                    <ShoppingCartIcon size={18} color='#FFD573' />
+                    <i className={`bi bi-cart ${styles.cart_icon}`}></i>
                     <p className={styles.itens}>X itens(s) no carrinho</p>
                 </div>
                 <div className={styles.total_pedido}>
-                    <p className={styles.total}>Total: R$ 5.00</p>
+                    <p className={styles.total}>Total: R$ 00.00</p>
                     <button className={styles.btn_finalizar}>
                         Finalizar pedido
-                        <ArrowRightIcon size={10} />
+                        <i className={`bi bi-arrow-right-short ${styles.arrow_right}`}></i>
                     </button>
                 </div>
             </section>
